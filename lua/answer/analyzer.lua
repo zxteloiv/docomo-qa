f6a7e20818cb7b696b4e8c8cbb9576881524ff1c
Xiang Zhang <zxteloiv@outlook.com>
2016-06-09 20:52:46 +0800

    change question data structure

--- lua/answer/analyzer.lua
+++ lua/answer/analyzer.lua
@@ -1,26 +1,27 @@
 local M_ = {}
 
+local POI_ATTR = {
+    NAME = false,
+    
+    LOCATION = {nil, nil},
+
+    ADDRESS = {
+        CITY = false,
+        DISTRICT = false,
+        STREET = false
+    },
+
+    OPEN_TIME = nil,
+    PRICE = nil,
+
+    DESC = nil,
+}
+
 -- question types
 local QTYPE = {
     UNKNOWN = 0,        -- questions that we cannot answer
-    POI_BY_NAME = 1,    -- find list of poi by name
 
-    POI_BY_CITY = 100,   -- find list of poi in city
-    POI_BY_DISTRICT = 101,   -- find pois in a district
-    POI_NEAR_PLACE = 102,  -- find poi near a place name or address
-
-    POI_BY_PRICE = 2,   -- find poi by price
-    POI_BY_OFFICE_HOUR = 3, -- find poi by office time
-
-    POI_BY_DESC = 5, -- find poi by description
-
-    POI_BY_FUNCTION = 6, -- find poi by function it has
-
-    ADDR_BY_POI_NAME = 1000,    -- find address by poi name
-    PRICE_BY_POI_NAME = 1001,   -- find average cost by poi name
-
-    SENTIMENTAL = 10000,    -- sentimental
-
+    SENTIMENTAL = false,
 }
 
 -- analyze a question, input the question string, and longitude and latitude in float numbers
