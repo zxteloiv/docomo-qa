e67f3330abaad02cbf766d04fc2eb3432881d292
Xiang Zhang <zxteloiv@outlook.com>
2016-06-22 15:05:59 +0800

    add content in empty rule to make it a real rule

--- lua/answer/rules/empty.lua
+++ lua/answer/rules/empty.lua
@@ -1,7 +1,14 @@
 local RULE_ = {}
 
+local qs = require("./lua/answer/query_schema")
+local ts = require("./lua/answer/template_schema")
+
 RULE_.match = function (question, query_repr) 
     ngx.say("in empty rule")
+
+    query_repr:set_qtype(qs.QTYPE.PIPELINE)
+    query_repr:add_input(qs.POI_ATTR.NAME, "forbidden city")
+    query_repr:add_input(qs.POI_ATTR.CITY, "beijing")
     return true
 end
 
