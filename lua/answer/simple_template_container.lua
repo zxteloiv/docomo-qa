df2d392eecb231ac5e78e7fd3c5ae602b15c2331
Xiang Zhang <zxteloiv@outlook.com>
2016-06-23 14:29:38 +0800

    add skeleton of template container

---
+++ lua/answer/simple_template_container.lua
@@ -0,0 +1,21 @@
+local Container = setmetatable({}, {
+    __call = function (cls, template)
+        return cls.new(template)
+    end,
+})
+
+Container.match = function()
+    assert(false, "container should not run directly")
+end
+
+Container.__index = Container
+Container.new = function(template)
+    local self = { rule = template }
+    setmetatable(self, Container)
+    return self
+end
+
+function Container:run (query_repr, question, lng, lat)
+end
+
+return Container
