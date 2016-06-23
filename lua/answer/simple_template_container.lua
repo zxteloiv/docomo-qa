b3d38ffbc6ca4f884e35a19732249a5f9130cd5d
Xiang Zhang <zxteloiv@outlook.com>
2016-06-23 16:37:48 +0800

    add the match for pure text

--- lua/answer/simple_template_container.lua
+++ lua/answer/simple_template_container.lua
@@ -1,3 +1,6 @@
+local ts = require("./lua/answer/template_schema")
+local qs = require("./lua/answer/query_schema")
+
 local Container = setmetatable({}, {
     __call = function (cls, template)
         return cls.new(template)
@@ -13,6 +16,42 @@ Container.new = function(template)
     local self = { rule = template }
     setmetatable(self, Container)
     return self
+end
+
+-- match type text
+-- Check if the string matches the text pattern, for the best performance
+--
+--  @str the input string
+--  @pattern the pattern to compare with
+--  @from the position index to start to compare, default to 1 if not given
+--
+--  @return a pair of word and position, if the pattern is matched,
+--     the word is the pattern itself and position is the index after the pattern.
+--     Otherwise, the returned word is nil and position is 0.
+--
+local function str_starts_with(str, pattern, from)
+    if not from then from = 1 end -- if from is missing, start from the beginning
+    local len = #pattern
+
+    local is_matched = true
+    for i = from, len do
+        if not str.byte[i] or not pattern.byte[i] then
+            is_matched = false
+            break
+        end
+        if str.byte[i] ~= pattern.byte[i] then
+            is_matched = false
+            break
+        end
+        
+    end
+
+    -- this tenary operator trick is tested here only and may not work elsewhere
+    local word = (is_matched and pattern or nil)
+    local pos = (is_matched and from + len or 0)
+    
+    return word, pos
+    
 end
 
 function Container:run (query_repr, question, lng, lat)
