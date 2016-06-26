79ff9e53c7ac93f36f5a69d812cc74df60fbc0c3
Xiang Zhang <zxteloiv@outlook.com>
2016-06-26 16:49:45 +0800

    re-indent source file

--- lua/utils/conf.lua
+++ lua/utils/conf.lua
@@ -1,36 +1,36 @@
 local conftool = {}
 
 conftool.open = function (conf_name)
-conf = {}
- 
-fp = io.open(conf_name, "r")
- 
-for line in fp:lines() do
-    line = line:match( "%s*(.+)" )
-    if line and line:sub( 1, 1 ) ~= "#" and line:sub( 1, 1 ) ~= ";" then
-     option = line:match( "%S+" ):lower()
-    value  = line:match( "%S*%s*(.*)" )
- 
-    if not value then
-         conf[option] = true
-    else
-        if not value:find( "," ) then
-        conf[option] = value
-        else
-        value = value .. ","
-        conf[option] = {}
-        for entry in value:gmatch( "%s*(.-)," ) do
-            conf[option][#conf[option]+1] = entry
-        end
-        end
-    end
- 
-    end
-end
- 
-fp:close()
+    conf = {}
 
-return conf
+    fp = io.open(conf_name, "r")
+
+    for line in fp:lines() do
+        line = line:match( "%s*(.+)" )
+        if line and line:sub( 1, 1 ) ~= "#" and line:sub( 1, 1 ) ~= ";" then
+            option = line:match( "%S+" ):lower()
+            value  = line:match( "%S*%s*(.*)" )
+
+            if not value then
+                conf[option] = true
+            else
+                if not value:find( "," ) then
+                    conf[option] = value
+                else
+                    value = value .. ","
+                    conf[option] = {}
+                    for entry in value:gmatch( "%s*(.-)," ) do
+                        conf[option][#conf[option]+1] = entry
+                    end
+                end
+            end
+
+        end
+    end
+
+    fp:close()
+
+    return conf
 end
 
 return conftool
