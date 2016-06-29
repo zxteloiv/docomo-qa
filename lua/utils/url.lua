53bb7bc94319e2ca62d60c496abd7e23bef0eabe
Xiang Zhang <zxteloiv@outlook.com>
2016-06-29 15:39:14 +0800

    move function closure outside

--- lua/utils/url.lua
+++ lua/utils/url.lua
@@ -1,26 +1,31 @@
 local M_ = {}
 
-function decode(str)
+local function byte2str(h)
+    return string.char(tonumber(h,16))
+end
+
+function url_decode(str)
     str = string.gsub (str, "+", " ")
-    str = string.gsub (str, "%%(%x%x)", function(h)
-        return string.char(tonumber(h,16))
-    end)
+    str = string.gsub (str, "%%(%x%x)", byte2str)
     str = string.gsub (str, "\r\n", "\n")
     return str
 end
 
-function encode(str)
+local function str2byte(c)
+    return string.format ("%%%02X", string.byte(c))
+end
+
+function url_encode(str)
     if (str) then
         str = string.gsub (str, "\n", "\r\n")
-        str = string.gsub (str, "([^%w %-%_%.%~])",
-        function (c) return string.format ("%%%02X", string.byte(c)) end)
+        str = string.gsub (str, "([^%w %-%_%.%~])", str2byte)
         str = string.gsub (str, " ", "+")
     end
     return str    
 end
 
 -- export symbols
-M_.decode = decode
-M_.encode = encode
+M_.decode = url_decode
+M_.encode = url_encode
 
 return M_
