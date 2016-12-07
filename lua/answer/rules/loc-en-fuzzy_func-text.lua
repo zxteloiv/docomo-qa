fe6cb2303dde4cc1bfb2194ef27701916e1b76e3
zxteloiv <zxteloiv@gmail.com>
2016-12-08 02:27:21 +0800

    add new fuzzy match rule for docomo downstream

---
+++ lua/answer/rules/loc-en-fuzzy_func-text.lua
@@ -0,0 +1,27 @@
+local ts = require("./lua/answer/template_schema")
+local qs = require("./lua/answer/query_schema")
+
+local conf = {
+    match_type = ts.MATCH_TYPE.FUZZY,
+
+    units = {
+        {
+            tag = ts.UNIT_TYPE.RE,
+            content = "where is|where|address|addr|地址|地点",
+            output = qs.POI_ATTR.ADDRESS,
+        },
+
+        {
+            tag = ts.UNIT_TYPE.RE,
+            content = ".{3,}",
+            input = qs.POI_ATTR.TAG, -- only supported in docomo search
+        }
+    },
+
+    fills = { },
+
+    downstream = qs.DOWNSTREAM.DOCOMO
+}
+
+return conf
+
