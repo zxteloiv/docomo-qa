6bcfb54ed59c297b224c8343c2089f2f778ea68d
zxteloiv <zxteloiv@gmail.com>
2017-02-28 18:07:51 +0800

    repond to complex query

--- lua/answer/subsearcher/rule_searcher.lua
+++ lua/answer/subsearcher/rule_searcher.lua
@@ -70,21 +70,11 @@ local function match_with_func(func, question, lng, lat)
     end
 end
 
--- answer a question based on its analysis result and user's location
---
---  @analysis a dict output from query analyzer
---  @lng longitude of user's current location
---  @lat latitude of user's current location
---
---  @return an answer dict
---
-local function respond(analysis, lng, lat)
-    local query_repr = analysis.query_repr
-
+local function fill_poi_input_args(input_schema, input_value)
     local args = {}
     -- input type and values
-    for i, schema in ipairs(query_repr.input_schema) do
-        local val = query_repr.input_value[i]
+    for i, schema in ipairs(input_schema) do
+        local val = input_value[i]
         if schema == query_schema.POI_ATTR.NAME then
             args.name = val
         elseif schema == query_schema.POI_ATTR.COORDINATES then
@@ -99,18 +89,10 @@ local function respond(analysis, lng, lat)
         end
     end
 
-    -- downstream type
-    if query_repr.downstream == query_schema.DOWNSTREAM.BAIDU_MAP then
-        args.downstream = "baidu"
-    elseif query_repr.downstream == query_schema.DOWNSTREAM.DOCOMO then
-        args.downstream = "solr"
-    else
-        -- not specifying will be defaulted to baidu later in poi API
-    end
+    return args
+end
 
-
-    local res = ngx.location.capture('/api/geo/poi', { args = args })
-
+local function build_response_from_poi_result(res)
     -- check the returned value
     if not res or res.status ~= 200 then
         return {errno = 1, errmsg = "poi srv failed"}
@@ -125,8 +107,106 @@ local function respond(analysis, lng, lat)
 
     local rtn = { errno = 0, errmsg = '', data = {} }
     rtn.data = res.data
+    return rtn
+end
+
+-- answer a question based on its analysis result and user's location
+--
+--  @analysis a dict output from query analyzer
+--  @lng longitude of user's current location
+--  @lat latitude of user's current location
+--
+--  @return an answer dict
+--
+local function respond(analysis, lng, lat)
+    local query_repr = analysis.query_repr
+
+    local args = fill_poi_input_args(query_repr.input_schema, query_repr.input_value)
+
+    -- downstream type
+    if query_repr.downstream == query_schema.DOWNSTREAM.BAIDU_MAP then
+        args.downstream = "baidu"
+    elseif query_repr.downstream == query_schema.DOWNSTREAM.DOCOMO then
+        args.downstream = "solr"
+    else
+        -- not specifying will be defaulted to baidu later in poi API
+    end
+
+
+    local res = ngx.location.capture('/api/geo/poi', { args = args })
+    local rtn = build_response_from_poi_result(res)
     rtn.query_repr = analysis.query_repr
-    rtn.downstream_args = args
+    rtn.match_score = analysis.match_score
+
+    return rtn
+end
+
+-- answer a question based on its analysis result and user's location
+--
+--  @analysis a dict output from query analyzer
+--  @lng longitude of user's current location
+--  @lat latitude of user's current location
+--
+--  @return an answer dict
+--
+local function respond_complex(analysis, lng, lat)
+    local query_repr = analysis.query_repr
+    local input_pipe = query_repr.input_pipe
+
+    local rtn = { errno = 0, errmsg = '', data = {} }
+    local last_docs = {}
+    local beam_size = 20
+
+    ngx.log(ngx.DEBUG, "I'm starting to loop input_pipe, in respond_complex")
+
+    for id, input in ipairs(input_pipe) do
+        -- set args for this turn
+        local args = fill_poi_input_args(input.schema, input.value)
+        args.downstream = "solr" -- complex queries only supported on solr engine
+
+        local current_docs = {}
+        ngx.log(ngx.INFO, "\n===============\n" .. json.encode(last_docs) .. "\n============\n")
+        -- args name is not specified in input_pipe and not the first one
+        if (not args.name) and id > 1 then
+            ngx.log(ngx.DEBUG, "I'm going to iterate over last docsets")
+            for _, doc in ipairs(last_docs) do
+                args.name = doc.general_val
+
+                local res = ngx.location.capture('/api/geo/poi', {args = args})
+                local res = build_response_from_poi_result(res)
+                -- if any error occured, the result can be returned as errmsg
+                if res.errno ~= 0 then return res end
+
+                -- pruning to beamsize in advance 
+                for id, newdoc in ipairs(res.data) do if id <= beam_size then
+                    table.insert(current_docs, newdoc)
+                end end
+            end
+            
+            -- choose the top-beamsize items into last_docs
+            table.sort(current_docs, function (x, y) return x.score > y.score end)
+            last_docs = {}
+            for id, newdoc in ipairs(current_docs) do if id <= beam_size then
+                table.insert(last_docs, newdoc)
+            end end
+
+        else
+            ngx.log(ngx.DEBUG, "I'm going to retrieve docset for the first time")
+            local res = ngx.location.capture('/api/geo/poi', {args = args})
+            local res = build_response_from_poi_result(res)
+            -- if any error occured, the result can be returned as errmsg
+            if res.errno ~= 0 then return res end
+
+            -- choose the top-beamsize items into last_docs for the sorted docset
+            for id, newdoc in ipairs(res.data) do if id <= beam_size then
+                table.insert(current_docs, newdoc)
+            end end
+
+            last_docs = current_docs
+        end
+    end
+    rtn.data = last_docs
+    rtn.query_repr = analysis.query_repr
     rtn.match_score = analysis.match_score
 
     return rtn
@@ -154,7 +234,24 @@ local function main(GET)
     --
     local answer = {errno = 0, errmsg = 'tpl not matched'}
     if query_analysis.match_score and query_analysis.match_score > 0 then
-        answer = respond(query_analysis, lng, lat)
+
+        if #query_analysis.query_repr.input_pipe <= 1 then
+            -- query is simple
+            ngx.log(ngx.DEBUG, "I'm a SIMPLE query")
+            answer = respond(query_analysis, lng, lat)
+        else
+            -- query is complex
+            ngx.log(ngx.DEBUG, "I'm a COMPLEX query")
+            answer = respond_complex(query_analysis, lng, lat)
+        end
+    end
+
+    -- add template info into every returned doc
+    --
+    local req_type = "tpl"
+    if GET.req_type then req_type = GET.req_type end
+    for i, _ in ipairs(answer.data) do
+        answer.data[i][req_type] = GET[req_type]
     end
 
     ngx.say(json.encode(answer))
