de40f5de02743f33d5eb25d9ded5e49ff57d3b67
zxteloiv <zxteloiv@gmail.com>
2017-12-18 01:36:45 +0800

    add geo location option on web ui

--- statics/answer.js
+++ statics/answer.js
@@ -5,27 +5,49 @@ if (typeof jQuery === "undefined") {
 
 var handle_submit = function() {
     var query = $("#query").val();
+    var domain = $('#domain').val();
+    var city = $('#city').val();
+    var sex = $('#feature_sex').val();
+    var age = $('#feature_age').val();
+    var time = $('#time').val();
+    var geo_arr = $('#geo').val().split(',');
+
     $.ajax('/answer', {
         method: "POST",
         dataType: "json",
         data: {
             q: query,
-            lat: 39.983424,
-            lng: 116.322987,
+            domain: domain,
+            city: city,
+            sex: sex,
+            age: age,
+            time: time,
+            lat: geo_arr[1],
+            lng: geo_arr[0],
         },
         success: function(data, state, jqXHR) {
-            if (!data || !('errno' in data) || !('errmsg' in data) ||
-                    !('data' in data)) {
+            if (!data || !('errno' in data) || !('errmsg' in data)) {
                 console.log("returned data is not valid.");
+                $('#searchresult').text('').append($('<span>').addClass('label label-warning').text(
+                    'invalid data received'
+                ));
                 return;
             }
 
             if (data.errno > 0) {
                 console.log("server error: " + data.errno + "\n" + data.errmsg);
+                if (data.errno === 1) {
+                    $('#searchresult').text('').append($('<span>')
+                        .addClass('label label-danger').text( data.errmsg ));
+                } else {
+                    $('#searchresult').text('').append($('<span>').addClass('label label-warning').text(
+                        'Sorry, we can not understand your question.'
+                    ));
+                }
                 return;
             }
 
-            render_data(data.data, data.reprtype);
+            render_data(data.data);
         },
         error: function(jqXHR, state, err) {
             console.log(state + ": answer server api error\n" + err)
@@ -35,80 +57,52 @@ var handle_submit = function() {
     return false;
 };
 
-var render_data = function(pois, reprtype) {
-    $("#searchresult").html("")
-    for (var idx in pois) {
-        var poi = pois[idx];
+var render_data = function(pois) {
+    $("#searchresult").html("");
+    var poi_fieldnames = [
+        "名称", "地址", "简介",
+        "name", "addr", "class",
+        "分店", "营业时间", "电话",
+        "popularity",
+        "门票价格", "服务评分", "环境评分",
+        '推荐菜品', '人均消费',
+        "rating",
+        '入离店时间', '联系方式', '每晚最低价格',
+        '酒店设施','房间设施','酒店服务'
+    ];
+    var rename_fields = { popularity: '流行程度', name: '名称', addr: '地址', 'class': '分类', 'rating': '评价' };
 
+    pois.forEach(function(poi) {
         var row = $("<div>").addClass("row");
 
-        var name = $("<div>").addClass("col-md-2");
-        var zh_name = $("<span>");
-        if (poi.name) { zh_name.text(poi.name); }
-        else if (poi.zh) { zh_name.text(poi.zh); }
-        var en_name = $("<span>");
-        if (poi.en) { en_name.text(poi.en); }
-        var ja_name = $("<span>");
-        if (poi.ja) { ja_name.text(poi.ja); }
-        name.append(zh_name);
-        name.append('<br>').append(en_name);
-        name.append('<br>').append(ja_name);
+        poi_fieldnames.forEach(function(field) {
+            if (!poi.hasOwnProperty(field)) { return; }
+            var value = poi[field];
 
-        var addr = $("<span>").addClass("col-md-3");
-        if (poi.addr) {
-            addr.text(poi.addr);
-        }
+            if (!value || (value.trim && value.trim() === "")) { return; }
 
-        var rating = $("<span>").addClass("col-md-1");
-        if (poi.rating) {
-            rating.text(poi.rating);
-        }
+            var key_span = $('<span>').addClass('col-xs-2');
+            key_span.text(rename_fields.hasOwnProperty(field) ? rename_fields[field] : field);
+            var val_span = $('<span>').addClass('col-xs-10');
+            val_span.text(value);
 
-        var phone = $("<span>").addClass("col-md-1");
-        if (poi.phone) {
-            phone_str = poi.phone.replace(/[, \|]/g, '$& ');
-            phone.text(phone_str)
-        }
+            row.append(key_span);
+            row.append(val_span);
+            row.append('<br>');
 
-        var tag = $("<span>").addClass("col-md-1");
-        if (poi.class) {
-            tag.text(poi.class)
-        }
-
-        var loc = $("<span>").addClass("col-md-2");
-        if (poi.lng && poi.lat) {
-            loc.text(poi.lng + "," + pois[idx].lat)
-        }
-
-        var more = $("<span>").addClass("col-md-1");
-        if (poi.url) {
-            more.append($("<a>").attr("href", poi.url)
-                .attr("target", "_blank").text("more"));
-        }
-
-        row.append(name);
-        row.append(addr);
-        row.append(rating);
-        row.append(phone);
-        row.append(tag);
-        row.append(loc);
-        row.append(more);
+        });
 
         $("#searchresult").append(row);
-
-        if (poi.general_val && poi.general_func) {
-            $("#searchresult").append(
-                $("<div>").addClass("row").append(
-                    $("<span>").addClass("col-md-12")
-                    .text(poi.general_func + ": " + poi.general_val)
-                    )
-                )
-        }
-
         $("#searchresult").append("<hr>");
+    });
+
+    if (pois.length === 0) {
+        $("#searchresult").text('')
+            .append($('<span>').addClass('label-info label').text('No result found.'));
     }
 };
 
 +function($){
     $("#searchform").submit(handle_submit);
 }(jQuery);
+
